e91113ae4e98011fe8fc0e95863db51afa41531c
Will <willanjosaway@gmail.com>
2018-10-31 19:56:29 -0300

    atualizacao de  imports

--- src/test/java/HU7RelatorioProdutoTestErroUm.java
+++ src/test/java/HU7RelatorioProdutoTestErroUm.java
@@ -6,8 +6,7 @@ import java.nio.file.Paths;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.AfterAll;
-	import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import br.senai.sc.edu.projetomaria.service.CargaService;
